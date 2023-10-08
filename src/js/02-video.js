260e2f8c8289e88d0aa7dd80918562146d625c82
олена городюк <135028180+OlenaUser1982@users.noreply.github.com>
2023-10-08 19:32:09 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(value) {
+  localStorage.setItem('videoplayer-current-time', value.seconds);
+}
+const readDate = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(readDate);
